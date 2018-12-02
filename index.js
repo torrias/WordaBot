5f5c5e89bd9a95d7c26cb2c8879f35d8df269e65
Zakhar Pilipenko <36258587+torrias@users.noreply.github.com>
2018-12-02 23:56:07 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,13 @@
 const Telegraf = require('telegraf')
 const config = require('./config.json')
-const bot = new Telegraf(config.token, {polling: true})
-bot.start((ctx) => ctx.reply('Welcome'))
-bot.startPolling()
+const bot = new Telegraf(config.token)
+
+bot.use((ctx, next) => {
+  const start = new Date()
+  return next(ctx).then(() => {
+    const ms = new Date() - start
+    console.log('Response time %sms', ms)
+  })
+})
+
+bot.on('text', (ctx) => ctx.reply('Hello World'))
