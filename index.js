6ca16140dbb0f89121f2f67e94379e9fa60a4362
Zakhar Pilipenko <36258587+torrias@users.noreply.github.com>
2018-12-02 19:34:22 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,4 @@
 const Telegraf = require('telegraf')
 const token = '719010870:AAFUfj3ic-azFfzQRWrhDh9aQN4kC7VAz9w'
 const bot = new Telegraf(token, {polling: true})
-bot.start((ctx) => {
-    console.log('Id пользователя:', ctx.from.id);
-    return ctx.reply('Добро пожаловать!');
-  });
+bot.start((ctx) => ctx.reply('Welcome'))
