d0840a8517555864ad22a87f9c4f76bcf2cd7b3b
torrias <pilipenkoza@gmail.com>
2018-12-13 15:10:43 +0300

    попытка чтения слов

--- index.js
+++ index.js
@@ -1,18 +1,40 @@
 const Telegraf = require('telegraf')
 const config = require('./config.json')
 const bot = new Telegraf(config.token, {polling: true})
+bot.start((ctx) => {
+  console.log('Id пользователя:', ctx.from.id);
+  return ctx.reply('Добро пожаловать!');
+}); 
+//Добавление нового слова в словарь
+const session = require("telegraf/session");
+const Stage = require("telegraf/stage");
+const WizardScene = require("telegraf/scenes/wizard");
+var newword = new Array()
+const create = new WizardScene(
+  "addword", // Имя сцены
+  (ctx) => {
+    ctx.reply('Введите слово для заучивания');
+    newword[0] = ctx.message.text;
+    return ctx.wizard.next(); 
+  },
+  (ctx) => {
+    ctx.reply('Какой его перевод?');
+    newword[1] = ctx.message.text;
+    return ctx.wizard.next(); 
+  },
+  (ctx) => {
+    ctx.reply(newword[1]);
+    return ctx.scene.leave();
+  }
+);
+// Создаем менеджера сцен
+const stage = new Stage();
 
-bot.use((ctx, next) => {
-  const start = new Date()
-  return next(ctx).then(() => {
-    const ms = new Date() - start
-    console.log('Response time %sms', ms)
-  })
-})
-var space = ' '
-bot.on('text', (ctx) => {
-    var temp_text = ctx.message.from.username
-    var splits = temp_text.split(space)
-    ctx.reply('Занесено ' + (splits.length/2))
-})
-bot.startPolling()
+// Регистрируем сцену создания матча
+stage.register(create);
+
+bot.use(session());
+bot.use(stage.middleware());
+bot.command("addword", (ctx) => ctx.scene.enter("addword"));
+console.log(newword);
+bot.startPolling();
