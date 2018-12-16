461928d9c99675ae79b2e0aaa9a5591cc3919070
Zakhar Pilipenko <36258587+torrias@users.noreply.github.com>
2018-12-17 00:01:38 +0300

    update ts
    Есть подозрение, что после  bot.command('/addword',ctx =>  нужно писать функцию ввода слова и в ней повторно обращаться к интерфейсу бота.

--- dist/index.js
+++ dist/index.js
@@ -1,2 +1,34 @@
-console.log('hi');
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const dotenv = require("dotenv");
+const telegraf = require('telegraf');
+class word {
+    getInfo() {
+        return "Слово " + this.original + "переводится как " + this.translate;
+    }
+}
+dotenv.config({ path: `${__dirname}/../.env` });
+const bot = new telegraf(process.env.TOKEN);
+bot.start((ctx) => {
+    ctx.reply('Добро пожаловать в бот-словарик');
+    ctx.reply('Введи addword для добавления слова в словарь');
+});
+bot.command('/addword', ctx => {
+    ctx.reply('Введите слово, перевод которого хотите запомнить:');
+});
+let dictionary = new word();
+bot.on('text', (ctx) => {
+    dictionary.original = ctx.message.from.username;
+});
+bot.on('text', ctx => {
+    ctx.reply('Введите перевод для этого слова:');
+});
+bot.on('text', (ctx) => {
+    dictionary.translate = ctx.message.from.username;
+});
+bot.command('/listword', ctx => {
+    ctx.reply(dictionary.original + dictionary.translate);
+});
+bot.startPolling();
+console.log(process.env.TOKEN);
 //# sourceMappingURL=index.js.map
--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,35 @@
-console.log('hi')
+import * as dotenv from 'dotenv'
+import { Telegraf, ContextMessageUpdate } from 'telegraf'
+const telegraf = require('telegraf')
+class word {
+    original:string
+    translate:string
+    getInfo():string{
+        return "Слово " + this.original+"переводится как " + this.translate;
+    }
+}
+
+dotenv.config({ path: `${__dirname}/../.env` })
+const bot: Telegraf<ContextMessageUpdate> = new telegraf(process.env.TOKEN)
+bot.start((ctx) => {
+    ctx.reply('Добро пожаловать в бот-словарик')
+    ctx.reply('Введи addword для добавления слова в словарь')
+})
+bot.command('/addword',ctx => {
+    ctx.reply('Введите слово, перевод которого хотите запомнить:')
+})
+let dictionary: word = new word()
+bot.on('text', (ctx) => {
+    dictionary.original=ctx.message.from.username
+})
+bot.on('text',ctx => {
+    ctx.reply('Введите перевод для этого слова:')
+})
+bot.on('text', (ctx) => {
+    dictionary.translate=ctx.message.from.username
+})
+bot.command('/listword',ctx => {
+    ctx.reply(dictionary.original+dictionary.translate)
+})
+bot.startPolling()
+console.log(process.env.TOKEN)
